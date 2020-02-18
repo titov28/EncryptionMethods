d4780fc86a5bd8f86d80e7ba1508efcfd8417520
Viacheslav Titov <stitov28@gmail.com>
2020-02-18 14:04:26 +0300

    Add project

---
+++ EncryptionMethods/VigenereCipher.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionMethods
+{
+    public class VigenereCipher
+    {
+
+        char[,] keyCard;
+
+
+        public VigenereCipher(char[] alphabet, int offset)
+        {
+            InitKeyCard(alphabet, offset);
+        }
+
+        private void InitKeyCard(char[] alphabet, int offset)
+        {
+            int lenghtAlphabet = alphabet.Length;
+            keyCard = new char[lenghtAlphabet, lenghtAlphabet];
+
+
+            int count = 0;
+
+            for(int i = 0; i < lenghtAlphabet; i++)
+            {
+                for(int j = 0; j < lenghtAlphabet; j++)
+                {
+                    if(count > lenghtAlphabet - 1)
+                    {
+                        count = count - (lenghtAlphabet - 1) - 1;
+                    }
+
+                    keyCard[i, j] = alphabet[count];
+                    count++;
+                }
+
+                count += offset;
+            }
+
+        }
+
+
+        public void PrintKeyCard()
+        {
+            int lenght = keyCard.GetUpperBound(0) + 1;
+
+            Console.WriteLine();
+            for (int i = 0; i < lenght; i++)
+            {
+                for (int j = 0; j < lenght; j++)
+                {
+                    Console.Write($"{keyCard[i, j]} ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public string Encryption(string enterString, char[] key)
+        {
+            char[] enterStringCharArray = enterString.ToCharArray();
+            char[] exitStringCharArray = new char[enterStringCharArray.Length];
+
+            for(int i = 0, j = 0; i < enterStringCharArray.Length; i++, j++)
+            {
+                if(j == key.Length)
+                {
+                    j = 0;
+                }
+
+                exitStringCharArray[i] = GetEncriptionElement(enterStringCharArray[i], key[j]);
+            }
+
+            return new string(exitStringCharArray);
+        }
+
+        public string Dencryption(string enterString, char[] key)
+        {
+            char[] enterStringCharArray = enterString.ToCharArray();
+            char[] exitStringCharArray = new char[enterStringCharArray.Length];
+
+            for (int i = 0, j = 0; i < enterStringCharArray.Length; i++, j++)
+            {
+                if (j == key.Length)
+                {
+                    j = 0;
+                }
+
+                exitStringCharArray[i] = GetDencriptionElement(enterStringCharArray[i], key[j]);
+            }
+
+            return new string(exitStringCharArray);
+        }
+
+
+        private char GetEncriptionElement(char enterStringElem, char keyElem)
+        {
+            int indexEnterStringElem = -1;
+            int indexKeyElem = -1;
+
+            for (int i = 0; i < keyCard.GetUpperBound(0) + 1; i++)
+            {
+                if(indexEnterStringElem != -1 && indexKeyElem != -1)
+                {
+                    break;
+                }
+
+                if (enterStringElem == keyCard[0, i])
+                {
+                    indexEnterStringElem = i;
+                }
+
+                if(keyElem == keyCard[0, i])
+                {
+                    indexKeyElem = i;
+                }
+            }
+
+
+            return keyCard[indexEnterStringElem, indexKeyElem];
+        }
+
+        private char GetDencriptionElement(char enterStringElem, char keyElem)
+        {
+            int indexEnterStringElem = -1;
+            int indexKeyElem = -1;
+
+            for(int i = 0; i < keyCard.GetUpperBound(0) + 1; i++)
+            {
+                if (keyElem == keyCard[0, i])
+                {
+                    indexKeyElem = i;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < keyCard.GetUpperBound(0) + 1; i++)
+            {
+                if (enterStringElem == keyCard[indexKeyElem, i])
+                {
+                    indexEnterStringElem = i;
+                    break;
+                }
+            }
+
+            return keyCard[0, indexEnterStringElem];
+
+        }
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EncryptionMethods;
+
+namespace Main
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '};
+
+
+            char[] key = { 'A', 'B', 'C' };
+
+            string enterString = "ONE TWO";
+            VigenereCipher cipher = new VigenereCipher(alphabet, 1);
+
+            cipher.PrintKeyCard();
+
+            Console.WriteLine($"Enter string: {enterString}");
+
+            string exitString = cipher.Encryption(enterString, key);
+
+            Console.WriteLine($"Exit string: {exitString}");
+
+            enterString = null;
+
+            enterString = cipher.Dencryption(exitString, key);
+
+            Console.WriteLine($"Enter string: {enterString}");
+
+            Console.ReadLine();
+        }
+    }
+}
